3cb3bf7d41f761b7799b6847f233e03931c6aef3
mafi1996 <mafiulalam1998@gmail.com>
2025-04-13 00:12:49 +0600

    Getter and Setter

--- app/src/main/java/com/example/objectsclass/Employee.java
+++ app/src/main/java/com/example/objectsclass/Employee.java
@@ -2,8 +2,32 @@ package com.example.objectsclass;
 
 public class Employee {
 
-    String name;
-    String position;
-    float salary;
+  private String name;
+   private String position;
+   private float salary;
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
 }
--- app/src/main/java/com/example/objectsclass/MainActivity.java
+++ app/src/main/java/com/example/objectsclass/MainActivity.java
@@ -21,22 +21,22 @@ public class MainActivity extends AppCompatActivity {
         tvdisplay.setText("");
 
         Employee employee = new Employee();
-        employee.name = "jhon";
-        employee.position = "manager";
-        employee.salary = 40000;
+        employee.setName("Mafiul alam");
+        employee.setPosition("Android Developer");
+        employee.setSalary(10000);
 
         Employee employee1 = new Employee();
-        employee1.name = "Mafi";
-        employee1.position = "manager";
-        employee1.salary = 40000;
+        employee1.setName("Jhon");
+        employee1.setPosition("Web Developer");
+        employee1.setSalary(10000);
 
-        tvdisplay.append("\nName: "+employee.name);
-        tvdisplay.append("\nPosition: "+employee.position);
-        tvdisplay.append("\nSalary: "+employee.salary+"usd");
+        tvdisplay.append(employee.getName());
+        tvdisplay.append("\n"+employee.getPosition());
+        tvdisplay.append("\n"+employee.getSalary()+"USD");
 
-        tvdisplay.append("\n\nName: "+employee1.name);
-        tvdisplay.append("\nPosition: "+employee1.position);
-        tvdisplay.append("\nSalary: "+employee1.salary+"usd");
+        tvdisplay.append("\n\n"+employee1.getName());
+        tvdisplay.append("\n"+employee1.getPosition());
+        tvdisplay.append("\n"+employee1.getSalary()+"USD");
 
     }
 }
