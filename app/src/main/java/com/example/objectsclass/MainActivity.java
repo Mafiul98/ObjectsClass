0ec63a3978cb9d4a216f1b02932edbd2dedb1841
mafi1996 <mafiulalam1998@gmail.com>
2025-04-14 20:48:21 +0600

    Polymorphism is OOPS

--- app/src/main/java/com/example/objectsclass/MainActivity.java
+++ app/src/main/java/com/example/objectsclass/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.objectsclass;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvdisplay;
+    Employee Mafi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,56 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvdisplay=findViewById(R.id.tvdisplay);
         tvdisplay.setText("");
-/*
 
-        Employee FulltimeEmployee = new FulltimeEmployee("Mafiul","App Devolper",10000);
-        tvdisplay.append("Name: "+FulltimeEmployee.getName());
-        tvdisplay.append("\nMainSalary: "+FulltimeEmployee.getSalary());
-        tvdisplay.append("\nTax: "+FulltimeEmployee.calculateTex());
-        tvdisplay.append("\nFinalSalary: "+FulltimeEmployee.calculateFinalSalary());
-
-        Employee ParttimeEmployee = new ParttimeEmployee("John","Web Devolper",10000);
-        tvdisplay.append("\n\nName: "+ParttimeEmployee.getName());
-        tvdisplay.append("\nMainSalary: "+ParttimeEmployee.getSalary());
-        tvdisplay.append("\nTax: "+ParttimeEmployee.calculateTex());
-        tvdisplay.append("\nFinalSalary: "+ParttimeEmployee.calculateFinalSalary());
-
- */
-
-        Employee employee2  = new Employee("Mafi","Developer",10000) {
-            @Override
-            public float calculateTex() {
-                return getSalary()*5/100;
-            }
-
-            @Override
-            public float calculateFinalSalary() {
-                return getSalary()-calculateTex();
-            }
-        };
-
-        tvdisplay.append("\nName: "+employee2.getName());
-        tvdisplay.append("\nMainSalary: "+employee2.getSalary());
-        tvdisplay.append("\nTax: "+employee2.calculateTex());
-        tvdisplay.append("\nFinalSalary: "+employee2.calculateFinalSalary());
+     Mafi = new FulltimeEmployee("Mafi","Developer",10000);
+     tvdisplay.append("Nmae: "+Mafi.getName());
+     tvdisplay.append("\nPosition: "+Mafi.getPosition());
+     tvdisplay.append("\nSalary: "+Mafi.getSalary());
+     tvdisplay.append("\nTex: "+Mafi.calculateTex());
+     tvdisplay.append("\nFinalSalary: "+Mafi.calculateFinalSalary());
 
 
-        Employee employee1 = new Employee("Bill gates","CEO",20000) {
-            @Override
-            public float calculateTex() {
-                return getSalary()*10/100;
-            }
+     tvdisplay.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             tvdisplay.setText("");
+             Mafi = new ParttimeEmployee("Mafi","Manager",8000);
+             tvdisplay.append("Nmae: "+Mafi.getName());
+             tvdisplay.append("\nPosition: "+Mafi.getPosition());
+             tvdisplay.append("\nSalary: "+Mafi.getSalary());
+             tvdisplay.append("\nTex: "+Mafi.calculateTex());
+             tvdisplay.append("\nFinalSalary: "+Mafi.calculateFinalSalary());
 
-            @Override
-            public float calculateFinalSalary() {
-                return getSalary()-calculateTex();
-            }
-        };
 
-        tvdisplay.append("\n\nName: "+employee1.getName());
-        tvdisplay.append("\nMainSalary: "+employee1.getSalary());
-        tvdisplay.append("\nTax: "+employee1.calculateTex());
-        tvdisplay.append("\nFinalSalary: "+employee1.calculateFinalSalary());
+
+         }
+     });
+
 
 
     }
